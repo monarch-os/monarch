75ea351aaecc162be21e32f9f7d59884808fb1c2
y0no <2242427+y0no@users.noreply.github.com>
2025-11-24 15:26:48 +0100

    Fix 3.2 sync (#5)
    * Fix(waybar migration issue)
    
    * Fix(do not disturb was not executable)
    
    * Add preview to monarch theme
    
    * Fix(Apply https://github.com/basecamp/omarchy/pull/3492)
    
    * Remove useless configuration
    
    * Mute unalias if gau does not exists
    
    * Replace OMZ by Starship
    
    * Change keybinding to avoid conflict between copy/paste and window orientation
    
    * Fix capslock issue
    
    * Improve label on walker

--- install/config/config.sh
+++ install/config/config.sh
@@ -5,10 +5,8 @@ cp -R ~/.local/share/monarch/config/* ~/.config/
 # Use default RC from Monarch
 cp ~/.local/share/monarch/default/bashrc ~/.bashrc
 
-# Install ZSH & Oh My ZSH
+# Install ZSH
 cp ~/.local/share/monarch/default/zshrc ~/.zshrc
-[ ! -d ~/.ssh ] && mkdir ~/.ssh && chmod 700 ~/.ssh # Must exist for ssh-agent to work
-for plugin in colored-man-pages docker extract fzf mise npm terraform tmux zsh-autosuggestions zsh-completions zsh-syntax-highlighting ssh-agent z ; do zsh -c "source ~/.zshrc && omz plugin enable $plugin || true"; done
 
 # Change shell
 sudo chsh -s /bin/zsh ${USER}
