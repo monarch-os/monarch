273f2ace10ddf1611d889d4f0714e886ff946502
Yoann ONO <2242427+y0no@users.noreply.github.com>
2025-12-09 21:32:38 +0100

    Improve load_user_setup Update leviathan modules

---
+++ migrations/1764711007.sh
@@ -0,0 +1,5 @@
+echo "Update exegol setup script and leviathan config"
+
+cp $MONARCH_PATH/default/exegol/my-resources/setup/load_user_setup.sh ~/.exegol/my-resources/setup/load_user_setup.sh
+cp $MONARCH_PATH/default/exegol/my-resources/setup/leviathan/modules/* ~/.exegol/my-resources/setup/leviathan/modules/
+cp $MONARCH_PATH/default/exegol/my-resources/setup/leviathan/workflows/* ~/.exegol/my-resources/setup/leviathan/workflows/
