6b8096852615a1a2b5fc5b487b32a62fa49808ef
Yoann ONO <2242427+y0no@users.noreply.github.com>
2026-04-09 13:09:29 +0200

    Fix(Fix few migrations and remove useless ones)

--- migrations/1760434895.sh
+++
@@ -1,6 +0,0 @@
-echo "Change to omarchy-nvim package"
-monarch-pkg-drop omarchy-lazyvim
-monarch-pkg-add omarchy-nvim
-
-# Will trigger to overwrite configs or not to pickup new hot-reload themes
-omarchy-nvim-setup
--- migrations/1762210472.sh
+++ migrations/1762210472.sh
@@ -1,5 +1,6 @@
 echo "Change to monarch-nvim package"
 monarch-pkg-drop omarchy-nvim
+monarch-pkg-drop omarchy-lazyvim
 monarch-pkg-add monarch-nvim
 
 # Will trigger to overwrite configs or not to pickup new hot-reload themes
