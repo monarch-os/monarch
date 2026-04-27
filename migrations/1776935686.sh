485cbce3fcfb3ef0e8692ad2795ca9368f96e1d1
Yoann ONO <2242427+y0no@users.noreply.github.com>
2026-04-27 23:24:34 +0200

    Fix monarch-recover-internal-monitor.service migration

--- migrations/1776935686.sh
+++ migrations/1776935686.sh
@@ -3,7 +3,7 @@ echo "Recover the internal monitor at login when no external display is connecte
 SERVICE=monarch-recover-internal-monitor.service
 
 mkdir -p ~/.config/systemd/user
-cp $monarch_PATH/config/systemd/user/$SERVICE ~/.config/systemd/user/$SERVICE
+cp $MONARCH_PATH/config/systemd/user/$SERVICE ~/.config/systemd/user/$SERVICE
 
 systemctl --user daemon-reload
 systemctl --user enable $SERVICE
